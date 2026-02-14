91fb76ef07794beff788ec0f3bfdbe1b3de33225
Toni-sam <omitoguntoni111@gmail.com>
2026-02-13 21:34:54 -0800

    Carousel in Home Page

--- FirstChoice.js
+++ FirstChoice.js
@@ -1,1 +1,36 @@
-<button onClick="topFunction()" id="backToTop" title="To Top">Top</button>
+const slides = document.querySelectorAll('.slider img');
+// console.log("Slides found:", slides.length);
+
+const slider = document.querySelector('.slider');
+let index = 0;
+let interval;
+
+function autoSlide() {
+    index++;
+    if (index >= slides.length) {
+        index = 0;
+    }
+
+    slider.scrollTo({
+        left: slides[index].offsetLeft,
+        behavior: "smooth"
+        
+    });   
+}
+
+function startSlide() {
+    interval = setInterval(autoSlide, 6000);
+    console.log("Started");
+}
+
+function stopSlide() {
+    clearInterval(interval);
+    console.log("Stop");
+}
+
+slider.addEventListener("mouseenter", stopSlide);
+slider.addEventListener("mouseleave", startSlide);
+
+startSlide();
+
+{/* <div><button onClick="topFunction()" id="backToTop" title="To Top">Top</button></div> */}
